5a253c883fde3fbd45a3a7d7a4b0b253890ec523
Danny <dannybcreative@gmail.com>
2019-08-14 22:44:00 -0700

    main menu base functionality is complete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,83 @@
-const treatmentMenuLink = document.querySelector('.info-link'),
-  treatmentMenu = document.querySelector('.treatment-menu'),
-  menuClose = document.querySelector('.menu-close');
+const treatmentMenuLink = document.querySelectorAll('.info-link'),
+  treatmentMenu = document.querySelectorAll('.treatment-menu'),
+  menuClose = document.querySelectorAll('.menu-close'),
+  mainMenu = document.querySelector('.main-menu'),
+  mainMenuClose = document.querySelector('.main-menu-close'),
+  menuInfo = document.querySelector('.menu-info'),
+  subMenu = document.querySelector('.submenu'),
+  menuColRight = mainMenu.querySelector('.col-right'),
+  menuColLeft = mainMenu.querySelector('.col-left'),
+  submenuClose = document.querySelector('.submenu-close'),
+  menuToggle = document.querySelector('.menu-toggle');
 
-treatmentMenuLink.addEventListener('click', (e) => {
-  treatmentMenu.classList.add('show-menu');
-  // target.preventDefault();
+
+/* Treatment Menus */
+treatmentMenuLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    if (link === treatmentMenuLink[0]) {
+      // console.log('this is number 1');
+      treatmentMenu[0].classList.add('show-menu');
+    } else if (link === treatmentMenuLink[1]) {
+      // console.log('this is number 2');
+      treatmentMenu[1].classList.add('show-menu');
+    } else {
+      // console.log('all are closed');
+      treatmentMenu[0].classList.remove('show-menu');
+      treatmentMenu[1].classList.remove('show-menu');
+    }
+  })
 });
-menuClose.addEventListener('click', () => {
-  treatmentMenu.classList.remove('show-menu');
+
+/* Close Treatment Menus */
+menuClose.forEach((menu) => {
+  menu.addEventListener('click', () => {
+    if (menu === menuClose[0]) {
+      treatmentMenu[0].classList.remove('show-menu');
+    } else if (menu === menuClose[1]) {
+      treatmentMenu[1].classList.remove('show-menu');
+    }
+  })
 });
+
+/* Main Menu Activate */
+menuToggle.addEventListener('click', () => {
+  mainMenu.classList.remove('main-menu-return');
+  mainMenu.classList.add('main-menu-down');
+});
+
+/* Main Menu Return */
+mainMenuClose.addEventListener('click', () => {
+  mainMenu.classList.remove('main-menu-down');
+  mainMenu.classList.add('main-menu-return');
+  subMenu.classList.remove('menu-drop');
+  subMenu.classList.add('menu-up');
+});
+
+/* Main menu */
+window.addEventListener('DOMContentLoaded', () => {
+  windowPane();
+});
+window.addEventListener('resize', () => {
+  windowPane();
+});
+
+const windowPane = () => {
+  if (window.innerWidth >= 768) {
+    menuColLeft.appendChild(subMenu);
+  } else if (window.innerWidth <= 768) {
+    menuColRight.appendChild(subMenu);
+  }
+}
+
+menuInfo.addEventListener('click', (e) => {
+  const eTarget = e.target;
+  console.log(eTarget);
+  if (eTarget.dataset.nav["sub"]) {
+    subMenu.classList.remove('menu-up');
+    subMenu.classList.add('menu-drop');
+  }
+});
+submenuClose.addEventListener('click', () => {
+  subMenu.classList.remove('menu-drop');
+  subMenu.classList.add('menu-up');
+})
